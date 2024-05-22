c2507f6a2b8b7625f53eda8756eab183c3633d0f
Troispoils <gitlab.troispoils@gmail.com>
2024-05-22 20:01:35 +0200

    Update basicaly Quadtree class

---
+++ AC2RE.Tools/Finder/Bound.cs
@@ -0,0 +1,17 @@
+﻿namespace AC2RE.Tools;
+
+public class Bound
+{
+    public Bound(float xMin, float yMin, float xMax, float yMax)
+    {
+        this.xMin = xMin;
+        this.yMin = yMin;
+        this.xMax = xMax;
+        this.yMax = yMax;
+    }
+
+    public float xMin { get; set; }
+    public float xMax { get; set;}
+    public float yMin { get; set;}
+    public float yMax { get; set;}
+}
---
+++ AC2RE.Tools/Finder/QuadTree.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+
+namespace AC2RE.Tools;
+
+public class QuadTree
+{
+    public Bound bound { get; set; }
+    public int capacitie { get; set; }
+    public bool divided { get; set; }
+    public QuadTree? nw { get; set; }
+    public QuadTree? ne { get; set; }
+    public QuadTree? sw { get; set; }
+    public QuadTree? se { get; set; }
+    public List<Vector3> points { get; set; }
+
+    public QuadTree(Bound bound, int capacitie = 4) {
+        this.bound = bound;
+        this.capacitie = capacitie;
+        points = new List<Vector3>();
+        divided = false;
+    }
+
+    public void SubDivide() {
+        var xC = (bound.xMin + bound.xMax) / 2;
+        var yC = (bound.yMin + bound.yMax) / 2;
+
+        nw = new QuadTree(new Bound(bound.xMin, yC, xC, bound.yMax));
+        ne = new QuadTree(new Bound(xC, yC, bound.xMax, bound.yMax));
+        sw = new QuadTree(new Bound(bound.xMin, bound.yMin, xC, yC));
+        se = new QuadTree(new Bound(xC, bound.yMin, bound.xMax, yC));
+
+        divided = true;
+    }
+}
