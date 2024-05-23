009349986a6d326a6ffec25f2a552b082a55c749
TroispoilsGit <gitlab.troispoils@gmail.com>
2024-05-23 19:01:01 +0200

    Fix CreateVectorPoint.

--- AC2RE.Tools/HeightMap.cs
+++ AC2RE.Tools/HeightMap.cs
@@ -4,22 +4,22 @@ using System.Runtime.CompilerServices;
 
 namespace AC2RE.Tools {
     public class HeightMap {
-        private const int LANDBLOCK_COUNT = 255;
+        private const int LANDBLOCK_SIZE = 255;
         private const int BLOCK_SIZE = 17;
         private const int CELL_SIZE = 10;
-        private const int GLOBAL_SIZE = LANDBLOCK_COUNT * BLOCK_SIZE * CELL_SIZE;
+        private const int GLOBAL_SIZE = LANDBLOCK_SIZE * BLOCK_SIZE * CELL_SIZE;
 
         public DatReader cellReader;
 
         public HeightMap() {
-            cellReader = new(@"C:\DatFiles\cell_1.dat");
+            cellReader = new(@"/home/troispoils/Documents/DatFiles/cell_1.dat");
         }
 
         public Vector3[,] GeneratePositions() {
-            var points = new Vector3[LANDBLOCK_COUNT * (BLOCK_SIZE - 1), LANDBLOCK_COUNT * (BLOCK_SIZE - 1)];
+            var points = new Vector3[LANDBLOCK_SIZE * (BLOCK_SIZE - 1), LANDBLOCK_SIZE * (BLOCK_SIZE - 1)];
 
-            for (byte landy = 0; landy < LANDBLOCK_COUNT; landy++) {
-                for (byte landx = 0; landx < LANDBLOCK_COUNT; landx++) {
+            for (byte landy = 0; landy < LANDBLOCK_SIZE; landy++) {
+                for (byte landx = 0; landx < LANDBLOCK_SIZE; landx++) {
                     ProcessLandBlock(points, landx, landy);
                 }
             }
@@ -53,11 +53,13 @@ namespace AC2RE.Tools {
         }
 
         private Vector3 CreateVectorPoint(CLandBlockData landBlockData, byte landx, byte landy, int x, int y) {
-            int tmpx = (landx * BLOCK_SIZE + x) * CELL_SIZE;
-            int tmpy = GLOBAL_SIZE - ((landy * BLOCK_SIZE + y) * CELL_SIZE) - 1;
-            int tmpz = landBlockData.heights[x * BLOCK_SIZE + y];
+            //TODO: add CELL_SIZE for calculate real pitch or another position
+            var pos = x * BLOCK_SIZE + y;
+            var posX = landx * BLOCK_SIZE + x - landx;
+            var posY = (LANDBLOCK_SIZE * BLOCK_SIZE) - (landy * BLOCK_SIZE + y) - 1 - (LANDBLOCK_SIZE - landy - 1);
+            var posZ = landBlockData.heights[pos];
 
-            return new Vector3(tmpx, tmpy, tmpz);
+            return new Vector3(posX, posY, posZ);
         }
     }
 }
